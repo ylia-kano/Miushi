f8bea8c6eb4dc88d436f6e98ee4bbfaafa782eb1
ylia-kano <44111823+ylia-kano@users.noreply.github.com>
2020-02-20 18:03:44 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-var plus = function(number){
-    return  number.querySelector('.price').textContent = number.querySelector('.price').getAttribute('value')  * Number(number.querySelector("input[name='quantity']").value);
-};
 
-var minus = function(number){
-    return  number.querySelector('.price').textContent =  number.querySelector('.price').textContent - number.querySelector('.price').getAttribute('value');
-};
-
+/*
 document.addEventListener('click', function (e) {
     if (e.target.classList.contains("quantity-controls-max")) {
         ++e.target.parentElement.querySelector("input[name='quantity']").value;
@@ -20,3 +14,38 @@ document.addEventListener('click', function (e) {
         document.querySelector('.basket').setAttribute('title', 'Сумма: ' + e.target.parentElement.parentElement.parentElement.querySelector('.price').textContent + ' руб.');
     }
 });
+*/
+
+for (var i = 0; i < document.querySelectorAll('.quantity-controls-max').length; i++) {
+    document.querySelectorAll('.quantity-controls-max')[i].addEventListener('click', function (e) {
+        plus(e.target.parentElement);
+    });
+}
+
+for (var i = 0; i < document.querySelectorAll('.quantity-controls-min').length; i++) {
+    document.querySelectorAll('.quantity-controls-min')[i].addEventListener('click', function (e) {
+        minus(e.target.parentElement);
+    });
+}
+
+for (var i = 0; i < document.querySelectorAll('.product__list--button-by').length; i++) {
+    document.querySelectorAll('.product__list--button-by')[i].addEventListener('click', function (e) {
+        this.textContent = 'В корзине';
+        this.style.background = '#2dac18';
+        document.querySelector('.basket-mini').textContent = e.target.parentElement.parentElement.parentElement.querySelector("input[name='quantity']").value;
+        document.querySelector('.basket').setAttribute('title', 'Сумма: ' + e.target.parentElement.parentElement.parentElement.querySelector('.price').textContent + ' руб.');
+    });
+}
+
+var plus = function (number) {
+    ++number.querySelector("input[name='quantity']").value;
+    return  number.parentElement.querySelector('.price').textContent = number.parentElement.querySelector('.price').getAttribute('value')  * Number(number.querySelector("input[name='quantity']").value);
+};
+
+var minus = function (number) {
+    if(number.querySelector("input[name='quantity']").value >1){
+        --number.querySelector("input[name='quantity']").value;
+        return  number.parentElement.querySelector('.price').textContent =  number.parentElement.querySelector('.price').textContent - number.parentElement.querySelector('.price').getAttribute('value');
+    }
+    
+};
